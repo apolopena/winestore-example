ddb84a285b759ff7f2d7b421ea78959c35353a3d
apolopena <penavilla@gmail.com>
2021-05-08 19:48:11 +0000

    ➕ ADD: project setup

--- .gp/bash/init-project.sh
+++ .gp/bash/init-project.sh
@@ -8,23 +8,36 @@
 # Initial configuration for an existing phpmyadmin installation.
 
 
-# Load logger
+# Load shared deps
 . .gp/bash/workspace-init-logger.sh
+. .gp/bash/spinner.sh
 
-# BEGIN example code block - migrate database
-# COMMENT: Load spinner
-# . .gp/bash/spinner.sh
-# __migrate_msg="Migrating database"
-# log_silent "$__migrate_msg" && start_spinner "$__migrate_msg"
-# php artisan migrate
-# err_code=$?
-# if [ $err_code != 0 ]; then
-#  stop_spinner $err_code
-#  log -e "ERROR: Failed to migrate database"
-# else
-#  stop_spinner $err_code
-#  log "SUCCESS: migrated database"
-# fi
+# Migrate and seed 
+msg="Migrating and Seeding the 'laravel' database"
+log_silent "$msg" && start_spinner "$msg"
+if php artisan migrate:fresh --seed; then
+  stop_spinner 0 && log "SUCCESS: $msg"
+else
+  stop_spinner 1 && log -e "ERROR: $msg"
+  exit 1
+fi
 
-# BEGIN example code block - migrate database
+# Install and Configure GraphQL
+if [[ ! -f config/graphql.php ]]; then
+  msg="Installing Laravel GraphQL API rebing/graphql-laravel via Composer"
+  if composer require rebing/graphql-laravel; then
+    stop_spinner 0 && log "SUCCESS: $msg"
+    msg="Extracting GraphQL configuration file to config/graphql.php"
+    if php artisan vendor:publish --provider="Rebing\GraphQL\GraphQLServiceProvider"; then
+      stop_spinner 0 && log "SUCCESS: $msg"
+    else
+      stop_spinner 1 && log -e "ERROR: $msg" && exit 1
+    fi # end extract rebing/graphql-laravel config
+  else
+    stop_spinner 1 && log -e "ERROR: $msg" && exit 1
+  fi # end install rebing/graphql-laravel
+else
+  msg="Installing Laravel dependencies"
+  composer install
+fi
 
